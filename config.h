ddb17e469dac202a3be1b556a92f19eebd1d6905
Kho-Dialga <ivandashenyou@gmail.com>
2020-08-04 09:05:49 -0600

    Galculator is now floating

--- config.h
+++ config.h
@@ -39,6 +39,7 @@ static const Rule rules[] = {
 	{ "St",      	     NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "Pinentry-gtk-2",  NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Alacritty",       NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "Galculator",      NULL,     NULL,           0,         1,          1,           0,        -1 },
 	{ NULL,              NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
