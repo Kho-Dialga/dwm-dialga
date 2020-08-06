7f748159b5ac069967eafef0d9512ccb7094e0ac
Kho-Dialga <ivandashenyou@gmail.com>
2020-08-06 10:28:51 -0600

    dragon is now floating now

--- config.h
+++ config.h
@@ -40,7 +40,7 @@ static const Rule rules[] = {
 	{ "Pinentry-gtk-2",  		NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Alacritty",       		NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "Galculator",      		NULL,     NULL,           0,         1,          1,           0,        -1 },
-	{ "dragon-drag-and-drop",       NULL,     NULL,           0,         1,          1,           0,        -1 },
+	{ "Dragon-drag-and-drop",       NULL,     NULL,           0,         1,          1,           0,        -1 },
 	{ NULL,              		NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
