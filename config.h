5da4b730efda7bf61ef7b50f56f04ea9aeac4eeb
Kho-Dialga <ivandashenyou@gmail.com>
2020-08-10 20:30:36 -0600

    Adjusted the gaps a bit

--- config.h
+++ config.h
@@ -4,10 +4,10 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 17;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 17;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 17;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 17;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
