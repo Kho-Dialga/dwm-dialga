e2a236a1b252a63114e8d573e002f7671d3a46ce
Kho-Dialga <ivandashenyou@gmail.com>
2020-09-04 08:40:14 -0600

    windows titled mpvfloat are floating

--- config.h
+++ config.h
@@ -41,6 +41,7 @@ static const Rule rules[] = {
 	{ "Alacritty",       		NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "Galculator",      		NULL,     NULL,           0,         1,          1,           0,        -1 },
 	{ "Dragon-drag-and-drop",       NULL,     NULL,           0,         1,          1,           0,        -1 },
+	{ "NULL",       		NULL,     "mpvfloat",     0,         1,          1,           0,        -1 },
 	{ NULL,              		NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
